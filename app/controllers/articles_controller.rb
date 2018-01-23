b2b9899dd15c9d1cb2cf87e076a94a2a1037dd36
Alexis Chavez <achavez@michelada.io>
2018-01-23 12:48:35 -0600

    Add edit, updated, show and index

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,8 @@
 class ArticlesController < ApplicationController
+  def index
+    @articles = Article.all
+  end
+
   def new
     @article = Article.new
   end
@@ -7,13 +11,24 @@ class ArticlesController < ApplicationController
     @article = Article.new(article_params)
     if @article.save
       flash[:notice] = "Article was succesfully created"
-      redirect_to article_path(@article)
+      redirect_to @article
     else
       render 'new'
     end
   end
 
   def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(article_params)
+      flash[:notice] = 'Article was succesfully updated'
+      redirect_to article_path(@article)
+    else
+      render 'edit'
+    end
   end
 
   def show
