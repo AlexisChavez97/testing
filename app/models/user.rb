4ebd1d87e70d686e3982ce792532c0df84f87820
Alexis Chavez <achavez@michelada.io>
2018-04-18 14:47:49 -0500

    user-articles associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
+  before_save { self.email = email.downcase }
+  has_many :articles
   validates :username, presence: true, length: { minimum: 3, maximum: 25 },
                        uniqueness: { case_sensitive: false }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
