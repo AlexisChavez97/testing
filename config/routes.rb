f307fcd707d637f71486df5ef8f781952913b2b4
Alexis Chavez <achavez@michelada.io>
2018-07-10 18:19:30 -0500

    create show action and add gravatar image

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,15 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  resources :articles
   root 'static_pages#home'
+
   get '/about', to: 'static_pages#about'
   get '/new', to: 'articles#new'
   post '/new', to: 'articles#create'
+
+  resources :articles
+
+  get '/signup', to: 'users#new'
+
+  resources :users, except: [:new]
 end
